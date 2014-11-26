fd2699c953ab1c7f3215b6aaec309b6f7e245448
andela-amaborukoje <adebayo@andela.co>
2014-11-26 07:02:17 +0100

    Commit

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
 
 var cart = {
-addButton :     document.getElementById('addNewTask'),
-newTask    :    document.getElementById('addItem'),
-incompleteTask: document.getElementById('incompletedList'),
-completedTask:  document.getElementById('completedList'),
-listItem :          "",
-// confirmEntry:   function () {
-//                 var userItem = value.replace(/^\s+/, '').replace(/\s+$/, '');
-//                 if (user)    
-validate :  function (item){
+    addButton :     document.getElementById('addNewTask'),
+    newTask    :    document.getElementById('addItem'),
+    incompleteTask: document.getElementById('incompletedList'),
+    completedTask:  document.getElementById('completedList'),
+      
+    validate :  function (item){
 
-if (item.trim()===""){
-    alert('ERROR');
-}else {
-    return item;
-}
-},
-
-//                 }
-
-createNewItem: function() {
-          //          console.log(this.newTask.value);
-                var list = document.createElement('li');
-                var checkbox = document.createElement('input');
-                    checkbox.setAttribute('type', 'checkbox');
-                var label = document.createTextNode(this.newTask.value);
-                 
-                list.appendChild(checkbox);
-                list.appendChild(label);
-                return list;
-
+                if (item.trim()===""){
+                    alert('YOU NEED TO INPUT SOMETHING');
+                    }
+                else {
+                    return item;
+                    }
                 },
 
-addItem :        function() {
+    createNewItem: function() {
+              //          console.log(this.newTask.value);
+                    var list = document.createElement('li');
+                    var checkbox = document.createElement('input');
+                        checkbox.setAttribute('type', 'checkbox');
+                    var label = document.createTextNode(this.newTask.value);
+                     
+                    list.appendChild(checkbox);
+                    list.appendChild(label);
+                    return list;
+
+                    },
+
+    addItem :       function() {
                    var item = this.newTask.value;
                    console.log(item); 
                    if (this.validate(item)) {
@@ -42,41 +38,39 @@ addItem :        function() {
                    this.bindTaskEvents(listItem, cart.taskCompleted);
                    return this.listItem;
                    }
-                  
-                },
+                      
+                    },
 
- bindTaskEvents : function (taskListItem, checkBoxEventHandler){
-    
-                var checkBox = taskListItem.querySelector("input[type=checkbox]");
-                checkBox.onclick = checkBoxEventHandler;
-                console.log(checkBox);
-},
+     bindTaskEvents : function (taskListItem, checkBoxEventHandler){
+        
+                    var checkBox = taskListItem.querySelector("input[type=checkbox]");
+                    checkBox.onclick = checkBoxEventHandler;
+                    console.log(checkBox);
+                    },
 
- taskCompleted :function() {
-                var listItem = this.parentNode;
-                // console.log(this.completedTask);
-                cart.completedTask.appendChild(listItem);
-                cart.bindTaskEvents(listItem, cart.taskIncomplete);
-},
+     taskCompleted :function () {
+                    var listItem = this.parentNode;
+                    // console.log(this.completedTask);
+                    cart.completedTask.appendChild(listItem);
+                    cart.bindTaskEvents(listItem, cart.taskIncomplete);
+                    },
 
-taskIncomplete : function(){
-                var listItem = this.parentNode;
-                cart.incompleteTask.appendChild(listItem);
-                cart.bindTaskEvents(listItem, cart.taskCompleted);
+    taskIncomplete : function () {
+                    var listItem = this.parentNode;
+                    cart.incompleteTask.appendChild(listItem);
+                    cart.bindTaskEvents(listItem, cart.taskCompleted);
+                    },
 
-},
+    trasverseIncomplete: function () {
+                    for (var i = 0; i < cart.incompleteTask.children.length; i++) {
+                    cart.bindTaskEvents(cart.incompleteTask.children[i], cart.taskCompleted);
+                        };
 
-trasverseIncomplete:     function () {
-                for (var i = 0; i < cart.incompleteTask.children.length; i++) {
-                cart.bindTaskEvents(cart.incompleteTask.children[i], cart.taskCompleted);
-                };
-
-},
-trasverseComplete: function(){for (var i = 0; i < cart.completedTask.children.length; i++) {
-                cart.bindTaskEvents(cart.completedTask.children[i], cart.taskIncomplete);
-
-            };
-},
+                    },
+    trasverseComplete: function(){for (var i = 0; i < cart.completedTask.children.length; i++) {
+                    cart.bindTaskEvents(cart.completedTask.children[i], cart.taskIncomplete);
+                        };
+                    },
 
 
 };
